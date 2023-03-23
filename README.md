7666b5765cc7b59c669f6b2f3dc396ceaf53e8c1
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-03-23 12:33:46 -0700

    it isn't pretty but it works - minus the table links - work on that later

---
+++ README.md
@@ -0,0 +1,44 @@
+
+      # Read my node
+      
+      ## Description
+      
+      A node that takes your input and creates a readme.md file for your current project.
+      
+      ## Table of Contents
+      
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [License](#license)
+      - [Contributing](#contributing)
+      - [Tests](#tests)
+      - [Questions](#questions)
+      
+      ## Installation
+      
+      npm dependencies, then node index
+      
+      ## Usage
+      
+      Creating professional readmes with user inputs.
+      
+      ## License
+      
+      This project is licensed under the MIT License license. See the [LICENSE](https://opensource.org/licenses/MIT) file for details.
+      
+      ## Contributing
+      
+      ask kindly
+      
+      ## Tests
+      
+      sat psat or lsat
+      
+      ## Questions
+      
+      snail mail
+      
+      ## Contact
+      
+      If you have any questions about the repo, open an issue or contact banditheelerpdx directly at test@test.com.
+      
--- index.js
+++ index.js
@@ -1,14 +1,144 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const licenses = [
+    {
+        name: "MIT License",
+        url: "https://opensource.org/licenses/MIT",
+      },
+      {
+        name: "Apache License 2.0",
+        url: "https://opensource.org/licenses/Apache-2.0",
+      },
+      {
+        name: "GNU General Public License v3.0",
+        url: "https://www.gnu.org/licenses/gpl-3.0",
+      },
+];
+
 inquirer
     .prompt ([
         {
             type: 'input',
-            message: 'What is the title of your project?'
+            message: 'What is the title of your project?',
             name: 'projectName'
         },
         {
-            
-        }
+            type: 'input',
+            message: 'Provide a description of the nature and implementation of your project.',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions for installing your application.',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Describe the intended usage of your application.',
+            name: 'usage'
+        },
+        {
+            type: 'list',
+            message: 'Which license would you like to use?',
+            name: 'license',
+            choices: licenses.map((license) => license.name)
+        },
+        {
+            type: 'input',
+            message: 'Should the people choose to assist in your endeavors, how might they go about doing so?',
+            name: 'howToContribute'
+        },
+        {
+            type: 'input',
+            message: 'Is there anyone that you would like to acknowledge?',
+            name: 'contributing'
+        },
+        {
+           type: 'input',
+           message: 'Provide detailed instructions on the type of and method by which to test your application.',
+           name: 'testing' 
+        },
+        {
+            type: 'input',
+            message: 'Should the masses seek an audience with you to discuss your application, what and how are your preferred means of communicaiton?',
+            name: 'questions'
+        },
+        {
+            type: 'input',
+            message: 'What is your github username?',
+            name: 'userName'
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+            validate: function(input) {
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                if (emailRegex.test(input)) {
+                    return true;
+                } else {
+                    return 'Please enter a valid email...';
+                }
+            }
+        },
     ])
+    .then((answers) => {
+        const { projectName, description, installation, usage, license, howToContribute, contributing, testing, questions, userName, email } = answers;
+      
+        const markdownString = `
+      # ${projectName}
+      
+      ## Description
+      
+      ${description}
+      
+      ## Table of Contents
+      
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [License](#license)
+      - [Contributing](#contributing)
+      - [Tests](#tests)
+      - [Questions](#questions)
+      
+      ## Installation
+      
+      ${installation}
+      
+      ## Usage
+      
+      ${usage}
+      
+      ## License
+      
+      This project is licensed under the ${license} license. See the [LICENSE](${licenses.find(l => l.name === license).url}) file for details.
+      
+      ## Contributing
+      
+      ${howToContribute}
+      
+      ## Tests
+      
+      ${testing}
+      
+      ## Questions
+      
+      ${questions}
+      
+      ## Contact
+      
+      If you have any questions about the repo, open an issue or contact ${userName} directly at ${email}.
+      `;
+      
+        fs.writeFileSync('README.md', markdownString, (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Successfully created README.md file');
+          }
+        });
+
+      })
+      .catch((err) => console.error(err));
+
